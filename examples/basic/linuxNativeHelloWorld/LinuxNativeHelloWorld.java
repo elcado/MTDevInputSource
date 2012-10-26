0375a62315a363a028040f4f347f035ad22aeeb7
Frédéric Cadier <f.cadier@free.fr>
2012-10-26 21:45:21 +0200

    Added code to properly close mtdev device on exit

--- examples/basic/linuxNativeHelloWorld/LinuxNativeHelloWorld.java
+++ examples/basic/linuxNativeHelloWorld/LinuxNativeHelloWorld.java
@@ -2,6 +2,7 @@ package basic.linuxNativeHelloWorld;
 
 import org.mt4j.MTApplication;
 import org.mt4j.input.InputManager;
+import org.mt4j.input.inputSources.AbstractInputSource;
 import org.mt4j.input.inputSources.MTDevInputSource;
 
 import basic.helloWorld.HelloWorldScene;
@@ -11,6 +12,25 @@ public class LinuxNativeHelloWorld extends MTApplication {
 		initialize();
 	}
 
+	@Override
+	public void exit() {
+		AbstractInputSource mtdevInputSource = null;
+		InputManager inputManager = this.getInputManager();
+		
+		// find mtdev input source
+		for (AbstractInputSource inputSource : inputManager.getInputSources()) {
+			if (inputSource instanceof MTDevInputSource) {
+				mtdevInputSource = inputSource;
+			}	
+		}
+		
+		// unregister mtdev input source
+		if (mtdevInputSource != null)
+			inputManager.unregisterInputSource(mtdevInputSource);
+		
+		super.exit();
+	}
+	
 	@Override
 	public void startUp() {
 		InputManager inputManager = this.getInputManager();
