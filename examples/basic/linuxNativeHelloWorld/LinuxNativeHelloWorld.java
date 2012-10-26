a001eb18622528c34e071f493bafb7c0b973771d
Frédéric Cadier <f.cadier@free.fr>
2012-10-26 21:36:52 +0200

    Moved test code

--- examples/basic/linuxNativeHelloWorld/LinuxNativeHelloWorld.java
+++ examples/basic/linuxNativeHelloWorld/LinuxNativeHelloWorld.java
@@ -1,12 +1,12 @@
-package org.mt4j.input.inputSources;
+package basic.linuxNativeHelloWorld;
 
 import org.mt4j.MTApplication;
 import org.mt4j.input.InputManager;
+import org.mt4j.input.inputSources.MTDevInputSource;
 
 import basic.helloWorld.HelloWorldScene;
 
-public class Test extends MTApplication {
-	/** main */
+public class LinuxNativeHelloWorld extends MTApplication {
 	public static void main(String[] args) {
 		initialize();
 	}
@@ -15,9 +15,8 @@ public class Test extends MTApplication {
 	public void startUp() {
 		InputManager inputManager = this.getInputManager();
 		inputManager.registerInputSource(new MTDevInputSource(this, "/dev/input/event4"));
-//		inputManager.registerInputSource(new MTDevInputSource(this, "/dev/input/event25"));
 
 		// build scene
-		this.addScene(new HelloWorldScene(this, ""));
+		this.addScene(new HelloWorldScene(this, "Test - Linux native mtdev support"));
 	}
 }
