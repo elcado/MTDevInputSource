56695ad37419813740accbf749be0e2d4703e692
Frédéric Cadier <f.cadier@free.fr>
2012-10-27 12:01:17 +0200

    Added LGPL license

--- src/org/mt4j/input/inputSources/MTDevInputSource.java
+++ src/org/mt4j/input/inputSources/MTDevInputSource.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2012 Frédéric Cadier <f.cadier@free.fr>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package org.mt4j.input.inputSources;
 
 import java.util.ArrayList;
@@ -16,6 +33,11 @@ import org.mt4j.input.inputSources.MTDevInputSource.SYN_CONSTANT;
 import org.mt4j.util.logging.ILogger;
 import org.mt4j.util.logging.MTLoggerFactory;
 
+/**
+ * Linux native mtdev input source.
+ * 
+ * @author Frédéric Cadier
+ */
 public class MTDevInputSource extends AbstractInputSource implements Cmtdev4j {
 	/** The Constant logger. */
 	private static final ILogger logger = MTLoggerFactory.getLogger(MTDevInputSource.class.getName());
